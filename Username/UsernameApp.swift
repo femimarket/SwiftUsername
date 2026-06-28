f068c316897b6186d55d680656e31d3812c3efde
femimarket <BJTDhwzXnYx7TLwQdyhVoArwbQnNAMJj2gq5Zs5hpump@femi.market>
2026-06-28 17:00:23 +0100

    Initial Commit

---
+++ Username/UsernameApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsernameApp.swift
+//  Username
+//
+//  Created by u on 28/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct UsernameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
